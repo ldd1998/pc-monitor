1b88ddb90b85edd034121c82673580bf5e9d9952
ldd1998 <1193804498@qq.com>
2023-04-16 21:34:45 +0800

    第一次提交

---
+++ src/main/java/com/example/pcmonitor/PcMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.example.pcmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class PcMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcMonitorApplication.class, args);
+    }
+
+}
