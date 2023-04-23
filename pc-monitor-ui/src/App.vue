0ab3d32559553aa05b14f7e015cdc43362f56433
ldd1998 <1193804498@qq.com>
2023-04-23 18:55:57 +0800

    增加随机用户名

--- pc-monitor-ui/src/App.vue
+++ pc-monitor-ui/src/App.vue
@@ -29,18 +29,24 @@ export default {
     this.cpuUsage = 0;
     this.memUsage = 0;
     this.openSocket();
-    setInterval(this.reConnectWebSocket,1000)
+    setInterval(this.reConnectWebSocket, 1000)
   },
   methods: {
+    getUUID() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    },
     // 重试webSocket
-    reConnectWebSocket(){
-      if(this.webSocket != null){
-        if(this.webSocket.readyState != 1){
-          console.log("ws连接重试，当前状态"+this.webSocket.readyState)
+    reConnectWebSocket() {
+      if (this.webSocket != null) {
+        if (this.webSocket.readyState != 1) {
+          console.log("ws连接重试，当前状态" + this.webSocket.readyState)
           this.webSocketClose()
           this.openSocket()
         }
-      }else {
+      } else {
         this.openSocket()
       }
     },
@@ -48,7 +54,7 @@ export default {
       if (typeof (WebSocket) === "undefined") {
         this.$toast.fail("您的浏览器不支持WebSocket")
       } else {
-        let SocketUrl = `ws://192.168.1.100:8081/monitor/ldd`
+        let SocketUrl = `ws://192.168.1.100:8081/monitor/` + this.getUUID()
         // 实例化WebSocket
         this.webSocket = new WebSocket(SocketUrl)
         // 监听socket连接
@@ -58,47 +64,47 @@ export default {
         // 监听socket消息
         this.webSocket.onmessage = this.webSocketOnMessage
         this.webSocket.onclose = this.webSocketClose
-        }
-      },
-      //关闭日志
-      checkSocket() {
-        if (this.webSocket != null) {
-          this.webSocketClose();
-          this.res = "";
-          this.webSocket = null;
-        } else {
-          this.openSocket();
-        }
-      },
-      // 开启日志
-      startSocket() {
-        this.openSocket();
-      },
-      // 连接建立之后执行send方法发送数据
-      webSocketOnOpen() {
-      },
-      // 连接建立失败重连
-      webSocketOnError() {
-        this.res = "<div style='color: #18d035;font-size: 14px'>通道连接失败,尝试重新连接,静默等待....</div>";
-        console.log("连接发生错误")
-        // setInterval(this.reConnectWebSocket(),1000);
-      },
-      // 数据接收
-      webSocketOnMessage: function (event) {
-        let sysInfo = JSON.parse(event.data);
-        this.cpuUsage = sysInfo.cpuUsed
-        this.memUsage = sysInfo.memUsed
-      },
-      // 数据发送
-      webSocketSend(Data) {
-        this.webSocket.send(Data);
-      },
-      //关闭
-      webSocketClose() {
-        console.log("webSocket关闭")
-        this.webSocket.close();
-      },
+      }
     },
+    //关闭日志
+    checkSocket() {
+      if (this.webSocket != null) {
+        this.webSocketClose();
+        this.res = "";
+        this.webSocket = null;
+      } else {
+        this.openSocket();
+      }
+    },
+    // 开启日志
+    startSocket() {
+      this.openSocket();
+    },
+    // 连接建立之后执行send方法发送数据
+    webSocketOnOpen() {
+    },
+    // 连接建立失败重连
+    webSocketOnError() {
+      this.res = "<div style='color: #18d035;font-size: 14px'>通道连接失败,尝试重新连接,静默等待....</div>";
+      console.log("连接发生错误")
+      // setInterval(this.reConnectWebSocket(),1000);
+    },
+    // 数据接收
+    webSocketOnMessage: function (event) {
+      let sysInfo = JSON.parse(event.data);
+      this.cpuUsage = sysInfo.cpuUsed
+      this.memUsage = sysInfo.memUsed
+    },
+    // 数据发送
+    webSocketSend(Data) {
+      this.webSocket.send(Data);
+    },
+    //关闭
+    webSocketClose() {
+      console.log("webSocket关闭")
+      this.webSocket.close();
+    },
+  },
 }
 </script>
 
@@ -107,13 +113,15 @@ export default {
   background-color: black;
   margin: 50px;
 }
-#CpuMonitor{
-  float:left;
+
+#CpuMonitor {
+  float: left;
   margin-right: 100px;
   background-color: black;
 }
-#MemMonitor{
-  float:left;
+
+#MemMonitor {
+  float: left;
   background-color: black;
 }
 
